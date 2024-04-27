357abad1030407cb712e63cc794aece2fdfe6ed6
Javier Rentas <javy.rentas@gmail.com>
2024-04-26 21:31:07 -0600

    Made a program that determines the grade of the user and whether they passed or not.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,70 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter your grade percentage: ");
+        string valueFromUser = Console.ReadLine();
+
+        int gradePercentage = int.Parse(valueFromUser);
+        string letter;
+        string sign;
+
+        if (gradePercentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradePercentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradePercentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradePercentage >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        if ((gradePercentage % 10) >= 7)
+        {
+            if (letter == "A" || letter == "F")
+            {
+                sign = "";
+            }
+            else
+            {
+            sign = "+";
+            }
+        } 
+        else if ((gradePercentage % 10) < 3)
+        {
+            if (letter == "F")
+            {
+                sign = "";
+            }
+            else
+            {
+            sign = "-";
+            }
+        }
+        else
+        {
+            sign = "";
+        }
+
+        Console.WriteLine($"Your grade: {letter}{sign}");
+
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed!");
+        }
+        else
+        {
+            Console.WriteLine("You did not pass. Try again, you got this!");
+        }
     }
 }
