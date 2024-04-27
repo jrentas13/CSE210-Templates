716b3a427a7b9df2d9d57ec58b6b4db5fe2339b1
Javier Rentas <javy.rentas@gmail.com>
2024-04-26 20:55:35 -0600

    Changed variable names to camel case.

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,10 +5,11 @@ class Program
     static void Main(string[] args)
     {
         Console.Write("What is your first name? ");
-        string first_name = Console.ReadLine();
-        Console.Write("What is your last name? ");
-        string last_name = Console.ReadLine();
+        string firstName = Console.ReadLine();
 
-        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}.");
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
