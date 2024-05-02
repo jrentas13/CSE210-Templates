2d923ab2526c3480be1448376c65f62ee8d77a63
Javier Rentas <javy.rentas@gmail.com>
2024-05-01 20:32:03 -0600

    Made functions that displayed a message, asked for the user's name, their favorite number, squared that number and displayed the results to the user.

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,46 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        int num = int.Parse(Console.ReadLine());
+        return num;
+    }
+
+    static int SquareNumber(int num)
+    {
+        int square = num * num;
+        return square;
+    }
+
+    static void DisplayResult(string name, int square)
+    {
+        Console.WriteLine($"{name}, the square of your number is {square}.");
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+
+        string name = PromptUserName();
+        int num = PromptUserNumber();
+
+        int square = SquareNumber(num);
+        
+        DisplayResult(name, square);
     }
 }
