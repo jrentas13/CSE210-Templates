959a6d4f7dbcaa701d4d2197b0c59f472dce7363
Javier Rentas <javy.rentas@gmail.com>
2024-06-18 13:33:35 -0600

    Created a program that calculates the are of different shapes.

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(new Square("yellow", 4));
+        shapes.Add(new Rectangle("red", 3, 5));
+        shapes.Add(new Circle("blue", 2));
+
+        foreach(Shape shape in shapes)
+        {
+            double area = shape.GetArea();
+            Console.WriteLine(area);
+        }
     }
 }
