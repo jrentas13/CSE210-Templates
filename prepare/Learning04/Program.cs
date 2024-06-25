8248bf249fbb861fbec527f7b0c00abed25665a4
Javier Rentas <javy.rentas@gmail.com>
2024-06-25 17:10:11 -0600

    Created an program that gives a summary of a math and a writng assignment.

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        MathAssignment math = new MathAssignment("Samuel Bennet", "Multiplication", "7.3", "8-19");
+        WritingAssignment history = new WritingAssignment("Mary Waters", "European History", "The Cause of World War II");
+
+        Console.WriteLine(math.GetSummary());
+        Console.WriteLine(math.GetHomewrokList());
+        Console.WriteLine(history.GetSummary());
+        Console.WriteLine(history.GetWritingInformation());
     }
 }
